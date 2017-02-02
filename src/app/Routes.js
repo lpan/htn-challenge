9fc5237352ba5ef5140ac652a461fe81292ccab2
Lawrence Pan <lawrencefeipan@gmail.com>
2017-02-02 14:20:43 -0500

    remove applicant page

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -4,14 +4,12 @@ import { Router, Route, Redirect } from 'react-router';
 import { history } from './store';
 import App from './App';
 import Applicants from './views/Applicants';
-import Applicant from './views/Applicant';
 
 const Routes = () => (
   <Router history={history}>
     <Redirect from="/" to="applicants" />
     <Route path="/" component={App}>
       <Route path="/applicants" component={Applicants} />
-      <Route path="/applicant/:appId" component={Applicant} />
     </Route>
   </Router>
 );
--- src/app/views/Applicant/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Applicant = () => (
-  <p>Applicant</p>
-);
-
-export default Applicant;
